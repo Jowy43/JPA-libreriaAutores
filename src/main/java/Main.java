4acf3af442e499f3e09ea533c87c41c702827765
ATEIS\a20joelps <joelps1177@gmail.es>
2022-03-07 19:21:14 +0100

    h

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 
+import dao.Autores;
 import java.util.Scanner;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main {
 
+    private static EntityManager manager;
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersistenciaMySQL");
+
     public static void main(String[] args) {
+        manager = emf.createEntityManager();
+        manager.getTransaction().begin();
+        
+        manager.getTransaction().commit();
+        manager.close();
 
     }
 
@@ -17,5 +29,17 @@ public class Main {
         System.out.println("6. Libros de un autor");
         System.out.println("7. Ver todos los libros");
         System.out.println("8. Todos los autores con sus libros");
+    }
+
+    private void crearLibro() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduce el DNI");
+        String dni = scan.nextLine();
+        System.out.println("Intorduce el nombre:");
+        String nombre = scan.nextLine();
+        System.out.println("Nacionalidad");
+        String nacionalidad = scan.nextLine();
+        Autores autor = new Autores(dni, nombre, nacionalidad);
+
     }
 }
